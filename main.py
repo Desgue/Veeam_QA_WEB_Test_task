730a05f3e30e6c53e71ceeec23af7c6fc3fc3cd5
Desgue <guedesgomero@gmail.com>
2024-03-15 19:32:58 +0000

    feat: add scrollarea scroll for department to select last possible options

--- main.py
+++ main.py
@@ -3,41 +3,95 @@ from selenium.webdriver.chrome.service import  Service
 from selenium.webdriver.common.by import  By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+
 
 def New_Driver():
     service = Service(executable_path =  'chromedriver.exe')
     driver = webdriver.Chrome(service = service)
     driver.get('https://careers.veeam.com/vacancies ')
     driver.maximize_window()
+    
     return driver
 
-def Get_Department_Toggler(driver):
+def Click_Department_Toggler(driver):
     toggler = driver.find_element(By.ID, 'department-toggler')
-    return toggler
+    toggler.click()
+    return
+
+def Select_Department(driver, department_name):
+    Click_Department_Toggler(driver)
+    area = Get_Scroll_Area(driver)[0]
+
+    for i in range(10):
+        driver.execute_script("arguments[0].scrollBy(0,50);", area) 
+        try:    
+            department = driver.find_element(By.LINK_TEXT, department_name)
+            department.click()
+            break
+        except Exception as e:
+            continue
+        
+
+
 
 
 # City and country toggler have both the same ID of 'city-toggler'
-def Get_Country_Toggler(driver):
-    toggler = driver.find_element(By.XPATH, '//*[@id="city-toggler"]')
+def Get_City_Togglers(driver):
+    # 0 = Country, 1 = City or State 2 = City if State is selected
+    toggler = driver.find_elements(By.XPATH, '//*[@id="city-toggler"]')
     return toggler
 
+# Some countries work, some countries throw error "Element not interactable" Countries valid up to "Costa Rica"
+# Problem might be visinilty of the element as we are dealing not with a select element but with a div element
+# FIX DEPARTMENT SELECTION WHEN FINDING A SOLUTION FOR COUNTRY SELECTION
+# ELEMENTS ARE ONLY VISIBLE UNTIL COSTA RICA CONFIRMING ASSUMPTION OF VISIBILITY
+def Get_Scroll_Area(driver):
+    scroll_area = driver.find_elements(By.XPATH, '//*[@class="scrollarea area"]')
+    return scroll_area
 
-def Select_Department(driver, department_name):
-    department_toggler = Get_Department_Toggler(driver)
-    department_toggler.click()
-    department = driver.find_element(By.XPATH, "//div[@class='scrollarea-content content']/a[contains(text(), '{}')]".format(department_name))
-    department.click()
-    return
 
+def Select_Country(driver, country_name):
+    country_toggler = Get_City_Togglers(driver)[0]
+    country_toggler.click()
 
+    wait = WebDriverWait(driver, 5)
+    scroll_area = Get_Scroll_Area(driver)[0]
+    for i in range(60):
+        driver.execute_script("arguments[0].scrollBy(0,500);", scroll_area) 
+        driver.implicitly_wait(2)
+    
+    
 
+    """ country = driver.find_element(By.LINK_TEXT, country_name)
+    actions = ActionChains(driver)
+    actions.move_to_element(country).perform()
+    country.click() """
+    
+        
 if __name__ == '__main__':
     driver = New_Driver()
-    Select_Department(driver, "Sales")
+    Select_Department(driver, "Purchasing")
 
+    time.sleep(5)
     
 
 
     driver.quit()
     pass
+
+# GAME PLAN
+# 1. Get the department toggler - DONE
+# 2. Click the department toggler - DONE
+# 3. Check if the department is visible
+# 4. If the department is visible, click the department
+# 5. If the department is not visible, scroll to the department and click the department
+# 6. Get the country toggler
+# 7. Check if the country is visible
+# 8. If the country is USA, I have to click the State toggler and select the state before the city
+# 9. If the country is not USA, I only have to click the city toggler
+# 10. Check if the city is visible
+# 11. If the city is visible, click the city
+# 12. If the city is not visible, scroll to the city and click the city
+# 13. Click the search button
