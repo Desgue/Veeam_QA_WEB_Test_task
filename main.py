79e26a105a2cf9c758bdb3843695326338e1f172
Desgue <guedesgomero@gmail.com>
2024-03-15 22:40:02 +0000

    add comment to main

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import scraper
+
+if __name__ == '__main__':
+    romania_options = scraper.Options(url="https://careers.veeam.com/vacancies",
+                            department="Sales",
+                            country="Romania",
+                            city="Bucharest",
+                            num_jobs_to_compare=28)
+    usa_options = scraper.Options(url="https://careers.veeam.com/vacancies",
+                            department="Sales",
+                            country="USA",
+                            state="Texas",
+                            city="Austin",
+                            num_jobs_to_compare=1)
+    romania_tester = scraper.Tester(romania_options)
+    usa_tester = scraper.Tester(usa_options)  
+
+    romania_tester.run()
+    usa_tester.run()
+
+
+### FIRST VERSION BEFORE REFACTOR BELOW, ONLY LEAVING IT HERE FOR COMPARISON PURPOSES ###
+
 """ from selenium import webdriver
 from selenium.webdriver.chrome.service import  Service
 from selenium.webdriver.common.by import  By
@@ -191,22 +214,3 @@ if __name__ == '__main__':
 # 4. ADD LOGGING
 # 5. TRANSFORM IN CLI TOOL """
 
-import scraper
-
-if __name__ == '__main__':
-    romania_options = scraper.Options(url="https://careers.veeam.com/vacancies",
-                            department="Sales",
-                            country="Romania",
-                            city="Bucharest",
-                            num_jobs_to_compare=28)
-    usa_options = scraper.Options(url="https://careers.veeam.com/vacancies",
-                            department="Sales",
-                            country="USA",
-                            state="Texas",
-                            city="Austin",
-                            num_jobs_to_compare=1)
-    romania_tester = scraper.Tester(romania_options)
-    usa_tester = scraper.Tester(usa_options)  
-
-    romania_tester.run()
-    usa_tester.run()
